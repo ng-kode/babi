10b04432cd743f6083317fa652866f180643e7a2
richard.ng <richard.chihong.ng@gmail.com>
2018-06-14 11:59:02 +0800

    named model layers

--- babi_memnn.py
+++ babi_memnn.py
@@ -152,26 +152,26 @@ print('-')
 print('Compiling...')
 
 # placeholders
-input_sequence = Input((story_maxlen,))
-question = Input((query_maxlen,))
+input_sequence = Input((story_maxlen,), name='story_inputs')
+question = Input((query_maxlen,), name='question_inputs')
 
 # encoders
 # embed the input sequence into a sequence of vectors
-input_encoder_m = Sequential()
+input_encoder_m = Sequential(name='story_m_embed_dropout')
 input_encoder_m.add(Embedding(input_dim=vocab_size,
                               output_dim=64))
 input_encoder_m.add(Dropout(0.3))
 # output: (samples, story_maxlen, embedding_dim)
 
 # embed the input into a sequence of vectors of size query_maxlen
-input_encoder_c = Sequential()
+input_encoder_c = Sequential(name='story_c_embed_dropout')
 input_encoder_c.add(Embedding(input_dim=vocab_size,
                               output_dim=query_maxlen))
 input_encoder_c.add(Dropout(0.3))
 # output: (samples, story_maxlen, query_maxlen)
 
 # embed the question into a sequence of vectors
-question_encoder = Sequential()
+question_encoder = Sequential(name='question_embed_dropout')
 question_encoder.add(Embedding(input_dim=vocab_size,
                                output_dim=64,
                                input_length=query_maxlen))
@@ -187,23 +187,23 @@ question_encoded = question_encoder(question)
 # compute a 'match' between the first input vector sequence
 # and the question vector sequence
 # shape: `(samples, story_maxlen, query_maxlen)`
-match = dot([input_encoded_m, question_encoded], axes=(2, 2))
+match = dot([input_encoded_m, question_encoded], axes=(2, 2), name='story_m_question_dot')
 match = Activation('softmax')(match)
 
 # add the match matrix with the second input vector sequence
-response = add([match, input_encoded_c])  # (samples, story_maxlen, query_maxlen)
-response = Permute((2, 1))(response)  # (samples, query_maxlen, story_maxlen)
+response = add([match, input_encoded_c], name='match_story_c_add')  # (samples, story_maxlen, query_maxlen)
+response = Permute((2, 1), name='match_permute')(response)  # (samples, query_maxlen, story_maxlen)
 
 # concatenate the match matrix with the question vector sequence
-answer = concatenate([response, question_encoded])
+answer = concatenate([response, question_encoded], name='match_question_concat')
 
 # the original paper uses a matrix multiplication for this reduction step.
 # we choose to use a RNN instead.
-answer = LSTM(32)(answer)  # (samples, 32)
+answer = LSTM(32, name='answer_lstm')(answer)  # (samples, 32)
 
 # one regularization layer -- more would probably be needed.
-answer = Dropout(0.3)(answer)
-answer = Dense(vocab_size)(answer)  # (samples, vocab_size)
+answer = Dropout(0.3, name='answer_dropout')(answer)
+answer = Dense(vocab_size, name='answer_dense')(answer)  # (samples, vocab_size)
 # we output a probability distribution over the vocabulary
 answer = Activation('softmax')(answer)
 
